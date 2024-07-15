a65cb0f9bf406afd40d10cbe544cabcac18f75de
Harsh Shah <shahmharsh@gmail.com>
2024-07-15 10:00:30 +0530

    Add documentation to APIs (#6)

--- client/lib/finvu_config.dart
+++ client/lib/finvu_config.dart
@@ -3,6 +3,11 @@ class FinvuConfig {
     required this.finvuEndpoint,
     this.certificatePins,
   });
+
+  /// The endpoint of the Finvu API.
   String finvuEndpoint;
+
+  /// The certificate pins to perform SSL pinning. These pins will need
+  /// to be updated regularly.
   List<String>? certificatePins;
 }
--- client/lib/finvu_manager.dart
+++ client/lib/finvu_manager.dart
@@ -21,6 +21,7 @@ class FinvuManager {
 
   get _platformExceptionTest => (e) => e is PlatformException;
 
+  /// Initializes the SDK with the [config]
   void initialize(final FinvuConfig config) {
     final NativeFinvuConfig nativeFinvuConfig = NativeFinvuConfig(
       finvuEndpoint: config.finvuEndpoint,
@@ -29,6 +30,7 @@ class FinvuManager {
     _nativeFinvuManager.initialize(nativeFinvuConfig);
   }
 
+  /// Connects to the Finvu AA server
   Future<void> connect() {
     return _nativeFinvuManager.connect().catchError(
           (e) => throw FinvuException.from(e),
@@ -36,10 +38,12 @@ class FinvuManager {
         );
   }
 
+  /// Disconnect from the Finvu AA server
   void disconnect() {
     _nativeFinvuManager.disconnect();
   }
 
+  /// Check if the SDK is connected to the Finvu AA server
   Future<bool> isConnected() {
     return _nativeFinvuManager.isConnected().catchError(
           (e) => throw FinvuException.from(e),
@@ -47,10 +51,17 @@ class FinvuManager {
         );
   }
 
+  /// Check if the SDK has an active session
   Future<bool> hasSession() {
     return _nativeFinvuManager.hasSession();
   }
 
+  /// Login with [username] or [mobileNumber].
+  /// One of [username] or [mobileNumber] is required. Calling this API will
+  /// initiate an OTP to users registered mobile number via the AA servers.
+  ///
+  /// [FinvuLoginOtpReference] is returned on success.
+  /// Thows [FinvuException] on failure.
   Future<FinvuLoginOtpReference>
       loginWithUsernameOrMobileNumberAndConsentHandle(
     String? username,
@@ -74,6 +85,11 @@ class FinvuManager {
         );
   }
 
+  /// Verify the [otp] received on the registered mobile number. [otpReference]
+  /// received in [loginWithUsernameOrMobileNumberAndConsentHandle]
+  ///
+  /// [FinvuHandleInfo] is returned on success, and session is established.
+  /// Thows [FinvuException] on failure.
   Future<FinvuHandleInfo> verifyLoginOtp(
     String otp,
     String otpReference,
@@ -89,6 +105,81 @@ class FinvuManager {
         );
   }
 
+  /// Initiates mobile verification for the given [mobileNumber]. Sometimes a
+  /// user might want to use a different mobile than the one used for logging in,
+  /// in which case they need to verify such a mobile before performing a request
+  /// like discovery. This API needs to be called within an authenticated session
+  /// (i.e. after [verifyLoginOtp]) to register additional mobiles with Finvu AA.
+  /// Calling this API will trigger an OTP to the given mobile number.
+  ///
+  /// Throws [FinvuException] on failure.
+  Future<void> initiateMobileVerification(String mobileNumber) {
+    return _nativeFinvuManager
+        .initiateMobileVerification(mobileNumber)
+        .catchError(
+          (e) => throw FinvuException.from(e),
+          test: _platformExceptionTest,
+        );
+  }
+
+  /// Completes the mobile verification process for the given [mobileNumber] and [otp].
+  /// The mobile number is only added for current session.
+  ///
+  /// Throws [FinvuException] on failure.
+  Future<void> completeMobileVerification(String mobileNumber, String otp) {
+    return _nativeFinvuManager
+        .completeMobileVerification(mobileNumber, otp)
+        .catchError(
+          (e) => throw FinvuException.from(e),
+          test: _platformExceptionTest,
+        );
+  }
+
+  /// Fetch the linked accounts for the current user. If there are no linked
+  /// accounts, account discovery and linking should be performed to discover
+  /// and link accounts, and then invoke this API again to get the list of
+  /// accounts to display for selecting the account for inclusion in the consent.
+  ///
+  /// Returns a list of [FinvuLinkedAccountDetailsInfo] on success.
+  /// Throws [FinvuException] on failure.
+  Future<List<FinvuLinkedAccountDetailsInfo>> fetchLinkedAccounts() {
+    return _nativeFinvuManager
+        .fetchLinkedAccounts()
+        .then(
+          (value) => value.linkedAccounts.nonNulls
+              .map(
+                (account) => FinvuLinkedAccountDetailsInfo(
+                  userId: account.userId,
+                  fipId: account.fipId,
+                  fipName: account.fipName,
+                  maskedAccountNumber: account.maskedAccountNumber,
+                  accountReferenceNumber: account.accountReferenceNumber,
+                  linkReferenceNumber: account.linkReferenceNumber,
+                  consentIdList: account.consentIdList?.nonNulls.toList(),
+                  fiType: account.fiType,
+                  accountType: account.accountType,
+                  linkedAccountUpdateTimestamp: account
+                              .linkedAccountUpdateTimestamp !=
+                          null
+                      ? DateTime.tryParse(account.linkedAccountUpdateTimestamp!)
+                      : null,
+                  authenticatorType: account.authenticatorType,
+                ),
+              )
+              .toList(),
+        )
+        .catchError(
+          (e) => throw FinvuException.from(e),
+          test: _platformExceptionTest,
+        );
+  }
+
+  /// Fetch the list of accounts that the user has with [fipDetails]. Accounts
+  /// will be discovered for all [fiTypes]. [identifiers] should be passed to
+  /// discover accounts as per REBIT spec for the [fiTypes].
+  ///
+  /// Returns a list of [FinvuDiscoveredAccountInfo] on success.
+  /// Throws [FinvuException] on failure.
   Future<List<FinvuDiscoveredAccountInfo>> discoverAccounts(
     FinvuFIPDetails fipDetails,
     List<String> fiTypes,
@@ -143,6 +234,11 @@ class FinvuManager {
         );
   }
 
+  /// API to initiate the account linking process for [accounts] with [fipDetails].
+  /// This API will trigger an OTP to the registered mobile number of the user.
+  ///
+  /// Returns a [FinvuAccountLinkingRequestReference] on success.
+  /// Throws [FinvuException] on failure.
   Future<FinvuAccountLinkingRequestReference> linkAccounts(
     FinvuFIPDetails fipDetails,
     List<FinvuDiscoveredAccountInfo> accounts,
@@ -189,6 +285,11 @@ class FinvuManager {
         );
   }
 
+  /// Confirm the account linking process with the [otp] received on the
+  /// registered mobile number and [requestReference] received in [linkAccounts]
+  ///
+  /// Returns a list of [FinvuLinkedAccountInfo] on success.
+  /// Throws [FinvuException] on failure.
   Future<FinvuConfirmAccountLinkingInfo> confirmAccountLinking(
     FinvuAccountLinkingRequestReference requestReference,
     String otp,
@@ -218,31 +319,48 @@ class FinvuManager {
         );
   }
 
-  Future<List<FinvuLinkedAccountDetailsInfo>> fetchLinkedAccounts() {
+  /// API to get consent requests raised by FIU with Finvu AA in a previous
+  /// server to server
+  ///
+  /// Returns a list of [FinvuConsentRequestInfo] on success.
+  /// Throws [FinvuException] on failure.
+  Future<FinvuConsentRequestDetailInfo> getConsentRequestDetails(
+    String handleId,
+  ) {
     return _nativeFinvuManager
-        .fetchLinkedAccounts()
+        .getConsentRequestDetails(handleId)
         .then(
-          (value) => value.linkedAccounts.nonNulls
-              .map(
-                (account) => FinvuLinkedAccountDetailsInfo(
-                  userId: account.userId,
-                  fipId: account.fipId,
-                  fipName: account.fipName,
-                  maskedAccountNumber: account.maskedAccountNumber,
-                  accountReferenceNumber: account.accountReferenceNumber,
-                  linkReferenceNumber: account.linkReferenceNumber,
-                  consentIdList: account.consentIdList?.nonNulls.toList(),
-                  fiType: account.fiType,
-                  accountType: account.accountType,
-                  linkedAccountUpdateTimestamp: account
-                              .linkedAccountUpdateTimestamp !=
-                          null
-                      ? DateTime.tryParse(account.linkedAccountUpdateTimestamp!)
-                      : null,
-                  authenticatorType: account.authenticatorType,
-                ),
-              )
-              .toList(),
+          (response) => FinvuConsentRequestDetailInfo(
+            consentId: response.consentId,
+            consentHandle: response.consentHandleId,
+            financialInformationUser: FinvuFinancialInformationEntityInfo(
+              id: response.financialInformationUser.id,
+              name: response.financialInformationUser.name,
+            ),
+            consentPurposeInfo: FinvuConsentPurposeInfo(
+              code: response.consentPurposeInfo.code,
+              text: response.consentPurposeInfo.text,
+            ),
+            consentDisplayDescriptions:
+                response.consentDisplayDescriptions.nonNulls.toList(),
+            dataDateTimeRange: FinvuDateTimeRange(
+              from: DateTime.tryParse(response.dataDateTimeRange.from),
+              to: DateTime.tryParse(response.dataDateTimeRange.to),
+            ),
+            consentDateTimeRange: FinvuDateTimeRange(
+              from: DateTime.tryParse(response.consentDateTimeRange.from),
+              to: DateTime.tryParse(response.consentDateTimeRange.to),
+            ),
+            consentDataFrequency: FinvuConsentDataFrequency(
+              unit: response.consentDataFrequency.unit,
+              value: response.consentDataFrequency.value,
+            ),
+            consentDataLifePeriod: FinvuConsentDataLifePeriod(
+              unit: response.consentDataLifePeriod.unit,
+              value: response.consentDataLifePeriod.value,
+            ),
+            fiTypes: response.fiTypes?.nonNulls.toList(),
+          ),
         )
         .catchError(
           (e) => throw FinvuException.from(e),
@@ -250,24 +368,12 @@ class FinvuManager {
         );
   }
 
-  Future<void> initiateMobileVerification(String mobileNumber) {
-    return _nativeFinvuManager
-        .initiateMobileVerification(mobileNumber)
-        .catchError(
-          (e) => throw FinvuException.from(e),
-          test: _platformExceptionTest,
-        );
-  }
-
-  Future<void> completeMobileVerification(String mobileNumber, String otp) {
-    return _nativeFinvuManager
-        .completeMobileVerification(mobileNumber, otp)
-        .catchError(
-          (e) => throw FinvuException.from(e),
-          test: _platformExceptionTest,
-        );
-  }
-
+  /// API to approve the consent request raised by FIU with Finvu AA. This API
+  /// should be called after the user has selected the [linkedAccounts] to be
+  /// included  in the [consentInfo].
+  ///
+  /// Returns a [FinvuProcessConsentRequestResponse] on success.
+  /// Throws [FinvuException] on failure.
   Future<FinvuProcessConsentRequestResponse> approveConsentRequest(
     FinvuConsentRequestDetailInfo consentInfo,
     List<FinvuLinkedAccountDetailsInfo> linkedAccounts,
@@ -341,6 +447,10 @@ class FinvuManager {
         );
   }
 
+  /// API to deny the consent request raised by FIU with Finvu AA.
+  ///
+  /// Returns a [FinvuProcessConsentRequestResponse] on success.
+  /// Throws [FinvuException] on failure.
   Future<FinvuProcessConsentRequestResponse> denyConsentRequest(
     FinvuConsentRequestDetailInfo consentInfo,
   ) {
@@ -394,7 +504,11 @@ class FinvuManager {
         );
   }
 
-  Future<void> revokeConsent(String consentId, FinvuUserConsentInfo consent, AccountAggregator? accountAggregator, FIPReference? fipDetails) {
+  /// API to revoke the consent for the given [consentId] and [consent].
+  ///
+  /// Throws [FinvuException] on failure.
+  Future<void> revokeConsent(String consentId, FinvuUserConsentInfo consent,
+      AccountAggregator? accountAggregator, FIPReference? fipDetails) {
     final nativeConsent = NativeUserConsentInfoDetails(
       consentId: consentId,
       consentIntentEntityId: consent.consentIntentEntityId,
@@ -405,15 +519,36 @@ class FinvuManager {
       consentPurposeText: consent.consentPurposeText,
       status: consent.status,
     );
-    final nativeAA = accountAggregator != null ? NativeAccountAggregator(id: accountAggregator.id!) : null;
-    final nativeFipDetails = fipDetails != null ? NativeFIPReference(fipId: fipDetails.fipId, fipName: fipDetails.fipName) : null;
+    final nativeAA = accountAggregator != null
+        ? NativeAccountAggregator(id: accountAggregator.id!)
+        : null;
+    final nativeFipDetails = fipDetails != null
+        ? NativeFIPReference(
+            fipId: fipDetails.fipId, fipName: fipDetails.fipName)
+        : null;
 
-    return _nativeFinvuManager.revokeConsent(nativeConsent, nativeAA, nativeFipDetails).catchError(
+    return _nativeFinvuManager
+        .revokeConsent(nativeConsent, nativeAA, nativeFipDetails)
+        .catchError(
           (e) => throw FinvuException.from(e),
           test: _platformExceptionTest,
         );
   }
 
+  /// Gets the status of the [handleId] after it has been approved by the user.
+  /// When the user approves a consent request, AA server will need to post the
+  /// consent to all the FIPs selected by the user. Once all FIPs have accepted
+  /// the consent posted by AA, AA server will change the status of the consent
+  /// [handleId] to READY. If any FIP rejects the consent, then the status will
+  /// change to REJECTED.
+  /// Note: This api can only be called a limited number of times, usually 5
+  /// times. If any attempts to call this api more than the allowed number of
+  /// times, the websocket session will be terminated and the connection will
+  /// become unusable. This is to prevent any runaway client process trying to
+  /// poll the consent handle status in an endless loop.
+  ///
+  /// Returns a [FinvuConsentHandleStatusResponse] on success.
+  /// Throws [FinvuException] on failure.
   Future<FinvuConsentHandleStatusResponse> getConsentHandleStatus(
     String handleId,
   ) {
@@ -430,52 +565,12 @@ class FinvuManager {
         );
   }
 
+  /// API to logout the user from the Finvu AA server. This will invalidate the
+  /// session and the user will need to login again to perform any further operations.
+  ///
+  /// Throws [FinvuException] on failure.
   Future<void> logout() {
     return _nativeFinvuManager.logout().catchError(
-          (e) => throw FinvuException.from(e),
-          test: _platformExceptionTest,
-        );
-  }
-
-  Future<FinvuConsentRequestDetailInfo> getConsentRequestDetails(
-    String handleId,
-  ) {
-    return _nativeFinvuManager
-        .getConsentRequestDetails(handleId)
-        .then(
-          (response) => FinvuConsentRequestDetailInfo(
-            consentId: response.consentId,
-            consentHandle: response.consentHandleId,
-            financialInformationUser: FinvuFinancialInformationEntityInfo(
-              id: response.financialInformationUser.id,
-              name: response.financialInformationUser.name,
-            ),
-            consentPurposeInfo: FinvuConsentPurposeInfo(
-              code: response.consentPurposeInfo.code,
-              text: response.consentPurposeInfo.text,
-            ),
-            consentDisplayDescriptions:
-                response.consentDisplayDescriptions.nonNulls.toList(),
-            dataDateTimeRange: FinvuDateTimeRange(
-              from: DateTime.tryParse(response.dataDateTimeRange.from),
-              to: DateTime.tryParse(response.dataDateTimeRange.to),
-            ),
-            consentDateTimeRange: FinvuDateTimeRange(
-              from: DateTime.tryParse(response.consentDateTimeRange.from),
-              to: DateTime.tryParse(response.consentDateTimeRange.to),
-            ),
-            consentDataFrequency: FinvuConsentDataFrequency(
-              unit: response.consentDataFrequency.unit,
-              value: response.consentDataFrequency.value,
-            ),
-            consentDataLifePeriod: FinvuConsentDataLifePeriod(
-              unit: response.consentDataLifePeriod.unit,
-              value: response.consentDataLifePeriod.value,
-            ),
-            fiTypes: response.fiTypes?.nonNulls.toList(),
-          ),
-        )
-        .catchError(
           (e) => throw FinvuException.from(e),
           test: _platformExceptionTest,
         );
