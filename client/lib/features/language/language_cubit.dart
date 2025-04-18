56dc319bca9b93df3d673b587af2e9d9ad97bfa0
yashwant <yashwantg@cookiejar.co.in>
2025-04-18 11:46:14 +0530

    locale changes

--- client/lib/features/language/language_cubit.dart
+++ client/lib/features/language/language_cubit.dart
@@ -6,15 +6,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LanguageCubit extends Cubit<Locale?> {
   LanguageCubit() : super(null);
 
-  void initialize() async {
-    final prefs = await SharedPreferences.getInstance();
-    final languageCode = prefs.getString('language') ?? 'en';
-    emit(Locale(languageCode));
+  void initialize({Locale? locale}) {
+    emit(locale ?? const Locale('en'));
   }
 
-  void changeLanguage(final Locale locale) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('language', locale.languageCode);
+  // Optional: if you want to allow runtime updates (not saved)
+  void changeLanguage(Locale locale) {
     emit(locale);
   }
 }
